422703924742306b88450a5245f2878da15b3476
Piotr Walerys <piotr.walerys@gmail.com>
2017-06-30 21:22:02 +0200

    Hiding Cell internals

--- cell.py
+++ cell.py
@@ -6,10 +6,10 @@ class State(Enum):
     ALIVE = 1
 
 class Cell:
-    MAX_NEIGHBORS = 8
+    NUMBER_OF_NEIGHBORS = 8
 
     def __init__(self, neighbors):
-        assert len(neighbors) == self.MAX_NEIGHBORS, "Cell has to have {} neighbors. Actual list: {}".format(self.MAX_NEIGHBORS, neighbors)
+        assert len(neighbors) == self.NUMBER_OF_NEIGHBORS, "Cell has to have {} neighbors. Actual list: {}".format(self.NUMBER_OF_NEIGHBORS, neighbors)
         self.neighbors = neighbors
         self.next_state = State.DEAD
         self.state = State.DEAD
@@ -21,3 +21,15 @@ class Cell:
     def update_state(self):
         self.state = self.next_state
 
+    @property
+    def alive(self):
+        return self.state == State.ALIVE
+
+    def set_alive(self):
+        self.state = State.ALIVE
+
+    def set_dead(self):
+        self.state = State.DEAD
+
+    def __str__(self):
+        return 'X' if self.alive else 'O'
--- test_cell.py
+++ test_cell.py
@@ -1,71 +1,71 @@
 import unittest
-from cell import Cell, State
+from cell import Cell
 
 class CellTest(unittest.TestCase):
 
     def setUp(self):
-        self.alive_cell = Cell([None]*Cell.MAX_NEIGHBORS)
-        self.alive_cell.state = State.ALIVE
+        self.alive_cell = Cell([None]*Cell.NUMBER_OF_NEIGHBORS)
+        self.alive_cell.set_alive()
 
-        self.dead_cell = Cell([None]*Cell.MAX_NEIGHBORS)
-        self.dead_cell.state = State.DEAD
+        self.dead_cell = Cell([None]*Cell.NUMBER_OF_NEIGHBORS)
+        self.dead_cell.set_dead()
 
         self.assertNotEqual(self.alive_cell, self.dead_cell)
 
     def test_next_state_will_be_dead_if_all_neighbors_are_dead(self):
-        cell = Cell([self.dead_cell]*Cell.MAX_NEIGHBORS)
-        cell.state = State.ALIVE
+        cell = Cell([self.dead_cell]*Cell.NUMBER_OF_NEIGHBORS)
+        cell.set_alive()
 
         cell.prepare_next_state()
         cell.update_state()
 
-        self.assertEqual(cell.state, State.DEAD)
+        self.assertFalse(cell.alive)
 
     def test_next_state_will_be_dead_if_all_neighbors_are_alive(self):
-        cell = Cell([self.alive_cell]*Cell.MAX_NEIGHBORS)
-        cell.state = State.ALIVE
+        cell = Cell([self.alive_cell]*Cell.NUMBER_OF_NEIGHBORS)
+        cell.set_alive()
 
         cell.prepare_next_state()
         cell.update_state()
 
-        self.assertEqual(cell.state, State.DEAD)
+        self.assertFalse(cell.alive)
 
     def test_cell_will_survive_if_two_neighbors_will_be_alive(self):
-        cell = Cell([self.alive_cell]*2 + [self.dead_cell]*(Cell.MAX_NEIGHBORS-2))
-        cell.state = State.ALIVE
+        cell = Cell([self.alive_cell]*2 + [self.dead_cell]*(Cell.NUMBER_OF_NEIGHBORS-2))
+        cell.set_alive()
 
         cell.prepare_next_state()
         cell.update_state()
 
-        self.assertEqual(cell.state, State.ALIVE)
+        self.assertTrue(cell.alive)
 
     def test_cell_will_survive_if_three_neighbors_will_be_alive(self):
-        cell = Cell([self.alive_cell]*3 + [self.dead_cell]*(Cell.MAX_NEIGHBORS-3))
-        cell.state = State.ALIVE
+        cell = Cell([self.alive_cell]*3 + [self.dead_cell]*(Cell.NUMBER_OF_NEIGHBORS-3))
+        cell.set_alive()
 
         cell.prepare_next_state()
         cell.update_state()
 
-        self.assertEqual(cell.state, State.ALIVE)
+        self.assertTrue(cell.alive)
 
     def test_cell_will_die_if_only_one_neighbor_will_be_alive(self):
-        cell = Cell([self.alive_cell]*1 + [self.dead_cell]*(Cell.MAX_NEIGHBORS-1))
-        cell.state = State.ALIVE
+        cell = Cell([self.alive_cell]*1 + [self.dead_cell]*(Cell.NUMBER_OF_NEIGHBORS-1))
+        cell.set_alive()
 
         cell.prepare_next_state()
         cell.update_state()
 
-        self.assertEqual(cell.state, State.DEAD)
+        self.assertFalse(cell.alive)
 
     def test_cell_will_die_if_more_than_three_neighbors_will_be_alive(self):
-        for alive, dead in zip(range(4, Cell.MAX_NEIGHBORS + 1), range(Cell.MAX_NEIGHBORS - 4, 3, -1)):
+        for alive, dead in zip(range(4, Cell.NUMBER_OF_NEIGHBORS + 1), range(Cell.NUMBER_OF_NEIGHBORS - 4, 3, -1)):
             cell = Cell([self.alive_cell]*alive + [self.dead_cell]*dead)
-            cell.state = State.ALIVE
+            cell.set_alive()
 
             cell.prepare_next_state()
             cell.update_state()
 
-            self.assertEqual(cell.state, State.DEAD)
+            self.assertFalse(cell.alive)
 
 
 if __name__ == "__main__":
